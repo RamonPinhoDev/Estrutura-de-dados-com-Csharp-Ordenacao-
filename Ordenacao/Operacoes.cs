aec8cba3a9cf75ac1fde07b3fd61f5bacc833768
Ramon Pinho <ramon-88@live.com>
2025-02-19 21:15:52 -0300

    first commit

---
+++ Ordenacao/Operacoes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordenacao
+{
+    internal class Operacoes
+    {
+
+        public int[] Ordenar(int[] array) 
+        {
+
+            for (int i = 1; i < array.Length -1; i++)
+            {
+                bool troca = false;
+               
+                    if (array[i] > array[i +1]) 
+                    {
+
+                    int marca = array[i];
+                    array[i] = array[i +1];
+                    array[i +1] = marca;
+                    troca = true;
+                    }
+            }
+            return array;
+        }
+    }
+}
---
+++ Ordenacao/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ordenacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] pontos = { 90,70,50,80,60,85};
+
+            var op = new Operacoes();
+
+            op.Ordenar(pontos);
+
+            foreach (int i in pontos) 
+            {
+                Console.Write(i+", ");
+            }
+
+          }
+    }
+}
